483320602d1686b52de3495562f295e404ef4f80
Hyerim Choi <hyerimc858@gmail.com>
2022-06-24 10:51:41 +0900

    Add: set data to IPFS

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	shell "github.com/ipfs/go-ipfs-api"
@@ -13,6 +15,7 @@ func main() {
 
 	router := gin.Default()
 	router.GET("/ipfs/:cid", getFromIPFS)
+	router.POST("/ipfs", setToIPFS)
 
 	router.Run("localhost:8000")
 }
@@ -34,4 +37,40 @@ func getFromIPFS(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"data": resp})
 
+}
+
+func setToIPFS(c *gin.Context) {
+
+	type ContentRequestBody struct {
+		Content string `json:"content"`
+	}
+
+	var requestBody ContentRequestBody
+
+	c.BindJSON(&requestBody)
+
+	cid, err := set(requestBody.Content)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s", err)
+		os.Exit(1)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"content_cid": cid})
+
+}
+
+func set(data string) (string, error) {
+	// Where your local node is running on localhost:5002
+	sh := shell.NewShell("localhost:5002")
+
+	cid, err := sh.Add(strings.NewReader(data))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s", err)
+		return "", err
+	}
+
+	fmt.Printf("added %s", cid)
+
+	return cid, nil
 }
